931244ed225cd049f6ef21095e57f4c8a90ffb64
juna1d9 <jahme014@gold.ac.uk>
2023-12-14 22:13:56 +0000

    Creating index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+// Import the modules we need
+var express = require("express");
+var ejs = require("ejs");
+var bodyParser = require("body-parser");
+
+// Create the express application object
+const app = express();
+const port = 8000;
+const mysql = require("mysql");
+const session = require("express-session");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "youth",
+  database: "employability",
+});
+// Connecting to the database
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to the database");
+});
+global.db = db;
+
+// Set up css
+app.use(express.static(__dirname + "/public"));
+
+// Set the directory where Express will pick up HTML files
+// __dirname will get the current directory
+app.set("views", __dirname + "/views");
+
+// Tell Express that we want to use EJS as the templating engine
+app.set("view engine", "ejs");
+
+// Tells Express how we should process html files
+// We want to use EJS's rendering engine
+app.engine("html", ejs.renderFile);
+
+// Define our data
+var blogData = { blogName: "Employability for the Youth" };
+
+// Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
+require("./routes/main")(app, blogData);
+
+// Start the web app listening
+app.listen(port, () => console.log("Example app listening on port ${port}!"));
+module.exports = app;
