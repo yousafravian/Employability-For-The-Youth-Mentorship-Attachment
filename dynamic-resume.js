937ff233c0dc05130966f73082f9b54b4e23013f
juna1d9 <jahme014@gold.ac.uk>
2024-04-12 16:25:30 +0500

    Job View & Detail Page + CV builder implemented

---
+++ dynamic-resume.js
@@ -0,0 +1,225 @@
+const dynamicResume = (options, themeOptions) => {
+    let dynamicHTML = `
+    <!doctype html>
+    <html lang="en">
+
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+            integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+        <title>Resume maker</title>
+        <style>
+            .resume {
+                width: 10.5in;
+                height: 10.5in;
+            }
+
+            .box {
+                background-color: ${themeOptions.wholeBodyColor};
+                width: 100%;
+                height: 100%;
+            }
+
+            .left-side {
+                color: ${themeOptions.leftTextColor};
+                width: 33%;
+                height: 100%;
+                background-color: ${themeOptions.leftBackgroundColor};
+
+            }
+
+            .right-side {
+                height: 100%;
+                width: 65%;
+                color: ${themeOptions.rightTextColor};
+            }
+
+            .name {
+                font-size: 1.2rem;
+            }
+
+            .profile-image {
+                width: 90%;
+                margin-left: 5%;
+                margin-top: 3%;
+            }
+
+            .profile-image img {
+                border-radius: 50%;
+            }
+
+            .heading-text {
+                font-size: 0.9rem;
+            }
+
+            .para,
+            .per-info {
+                font-size: 0.7rem !important;
+            }
+
+            .about .para {
+                width: 93%;
+            }
+
+            .key-skills li {
+                font-size: 0.7rem;
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <div class="resume border shadow d-flex aligh-items-center jusify-content-center">
+            <div class="box">
+                <!-- Your existing HTML structure -->
+                <div class="left-side d-inline-block">
+                    <div class="profile-image">
+                        <!--   <img class="img-fluid" src="https://pbs.twimg.com/media/ElkLaHCU4AIqplX?format=jpg" alt=""> -->
+                    </div>
+                    <div class="contact ml-2 mt-2">
+                        <div class="heading-text text-uppercase">Contact</div>
+                        <p class="para mb-1">
+                            ${options.address} <br>
+                            ${options.phone} <br>
+                            ${options.email} <br>
+                        </p>
+                    </div>
+                    <div class="expert ml-2 mt-2">
+                        <div class="heading-text text-uppercase">Expertise Area</div>
+                        <ul>
+                        `;
+
+                        options.expertise_area.forEach((expertise_area, index) => {
+                            dynamicHTML += `
+                                                    <li>${expertise_area}</li>`;
+                        });
+                    
+                        dynamicHTML += `</ul>
+                    </div>
+
+                    <div class="skill ml-2 mt-2">
+                        <div class="heading-text text-uppercase">Extra Skill</div>
+                        <ul>
+                        `;
+
+                        options.skills.forEach((skill, index) => {
+                            dynamicHTML += `
+                                                    <li>${skill}</li>`;
+                        });
+                    
+                        dynamicHTML += `</ul>
+                    </div>
+
+
+                    <div class="hobbies ml-2 mt-2">
+                        <div class="heading-text text-uppercase">hobbies</div>
+                        <p class="para mb-1">
+                        <ul>
+                        `;
+
+                        options.hobbies.forEach((hobby, index) => {
+                            dynamicHTML += `
+                                                    <li>${hobby}</li>`;
+                        });
+                    
+                        dynamicHTML += `</ul>
+                        </p>
+                    </div>
+                </div>
+                <div class="right-side d-inline-block m-0 p-0 align-top">
+                    <h2 class="name text-uppercase ml-3 my-2">${options.name}</h2>
+
+                    <div class="contact ml-3 mt-3">
+                        <div class="heading-text text-uppercase">About Me</div>
+                        <p class="para mb-1">
+                            ${options.aboutme}
+                        </p>
+                    </div>
+                    <div class="personal ml-3 mt-3">
+                        <div class="heading-text text-uppercase">Personal Informations</div>
+                        <table class="per-info">
+                            <tbody>
+                                <tr class="border">
+                                    <td>Father's Name </td>
+                                    <td>${options.fname}</td>
+                                </tr>
+                                <tr class="border">
+                                    <td>Mother's Name </td>
+                                    <td>${options.mname}</td>
+                                </tr>
+                                <tr class="border">
+                                    <td>Date of Birth </td>
+                                    <td>${options.dob}</td>
+                                </tr>
+                                <tr class="border">
+                                    <td>Address </td>
+                                    <td>${options.nationality}</td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+                    <div class="education ml-3 mt-3">
+                        <div class="heading-text text-uppercase">Educational information</div>
+                        <table class="per-info">
+                            <tbody>`;
+
+    options.educationalInformation.forEach((education, index) => {
+        dynamicHTML += `
+                                <tr class="border">
+                                    <td>${new Date(education.startYear).getFullYear()}-${new Date(education.endYear).getFullYear()}</td>
+                                    <td>
+                                        ${education.degree} <br>
+                                        ${education.university} <br>
+                                    </td>
+                                </tr>`;
+    });
+
+    dynamicHTML += `
+                            </tbody>
+                        </table>
+                    </div>
+
+
+
+                    <div class="key-skills ml-3 mt-3">
+                        <div class="heading-text text-uppercase">Professional Skills</div>
+                        <ul class="pl-1">
+                        `;
+
+                        options.key_skills.forEach((key_skill, index) => {
+                            dynamicHTML += `
+                                                    <li>${key_skill}</li>`;
+                        });
+                    
+                        dynamicHTML += `
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+            integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
+            crossorigin="anonymous"></script>
+    </body>
+
+    </html>
+    `;
+
+    return dynamicHTML;
+}
+
+module.exports = dynamicResume;
--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,12 @@
 const app = require("../index");
 const bcrypt = require("bcrypt");
+const dynamicResume = require('../dynamic-resume');
+const pdf = require('html-pdf');
+
+const options = {
+  "height": "10.5in",        // allowed units: mm, cm, in, px
+  "width": "8in",        // allowed units: mm, cm, in, pxI
+};
 
 module.exports = function (app) {
 
@@ -107,17 +114,178 @@ module.exports = function (app) {
     res.render("forgot.ejs");
   });
 
-  app.get("/applyforjobs", authenticateUser, function (req, res) {
-    res.render("applyforjobs.ejs");
+  app.get("/jobs", authenticateUser, function (req, res) {
+
+    const query = "SELECT * FROM jobs";
+    db.query(query, (error, results) => {
+      if (error) {
+        throw error;
+      }
+
+      if (results.length > 0) {
+
+        const jobs = results;
+        res.render("jobs.ejs", {jobs: jobs});
+
+      } else {
+
+        res.render("jobs.ejs", {
+          message: "Unable to find jobs!",
+        });
+
+      }
+    });
+
+  });
+
+  app.get("/jobs/:j_id", authenticateUser, function (req, res) {
+    const jobId = req.params.j_id;
+    const query = "SELECT * FROM jobs where j_id = ?";
+    db.query(query, [jobId], (error, results) => {
+      if (error) {
+        throw error;
+      }
+
+      if (results.length > 0) {
+
+        const job = results[0];
+        res.render("jobdetail.ejs", {job: job});
+
+      } else {
+
+        res.render("jobdetail.ejs", {
+          message: "Unable to find jobs!",
+        });
+        
+      }
+    });
+
   });
 
   app.get("/postajob", authenticateUser, function (req, res) {
     res.render("postajob.ejs");
   });
 
+  app.post("/postajob", authenticateUser, function (req, res) {
+    const {
+      j_cname,
+      j_email,
+      j_title,
+      j_description,
+      j_appdeadline,
+    } = req.body;
+
+    const query =
+      "INSERT INTO jobs (j_cname, j_email, j_title, j_description, j_appdeadline, j_u_id) VALUES (?, ?, ?, ?, ?, ?)";
+
+    const values = [
+      j_cname,
+      j_email,
+      j_title,
+      j_description,
+      j_appdeadline,
+      req.session.user.id
+    ];
+    db.query(query, values, (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.redirect("jobs.ejs")
+    });
+  });
+
   app.get("/C.V", authenticateUser, function (req, res) {
     res.render("c.v.ejs");
   });
+
+  app.get('/cv-builder/:theme', authenticateUser, (req, res, next) => {
+    console.log("theme: ", req.params.theme);
+    switch (req.params.theme) {
+        case '1':
+            res.render('cv-builder.ejs', { theme: "blue" });
+            break;
+        case '2':
+            res.render('cv-builder.ejs', { theme: "green" });
+            break;
+        default:
+            res.render('cv-builder.ejs', { theme: "green" });
+            break;
+    }
+});
+
+app.post('/cv-builder', authenticateUser, (req, res, next) => {
+  console.log(req.body.theme);
+  // LOWERCASE -> REMOVE SPACE -> SHORT NAME 
+  const userName = req.body.name;
+  const lowercaseName = userName.toLowerCase();
+  const noSpaceName = lowercaseName.replace(' ', '');
+  const shortName = noSpaceName.slice(0, 10);
+
+  //Educational Info
+  const { degrees, universities, startYears, endYears } = req.body;
+  const educationalInfo = [];
+
+  for (let i = 0; i < degrees.length; i++) {
+    const eduInfo = {
+      degree: degrees[i],
+      university: universities[i],
+      startYear: startYears[i],
+      endYear: endYears[i]
+    };
+    educationalInfo.push(eduInfo);
+  }
+
+  req.body.educationalInformation = educationalInfo;
+  console.log(req.body);
+
+
+  let themeOptions = {
+      leftTextColor: "rgb(91, 88, 255)",
+      leftBackgroundColor: 'rgb(12, 36, 58)',
+      wholeBodyColor: ' rgb(250, 250, 250)',
+      rightTextColor: 'rgb(12, 36, 58)'
+  };
+
+  if (req.body.theme === 'blue') {
+
+      // HTML TO PDF CONVERTING
+      pdf.create(dynamicResume(req.body, themeOptions), options).toFile(__dirname + "/docs/" + shortName + "-resume.pdf", (error, response) => {
+          if (error) throw Error("File is not created");
+          console.log(response.filename);
+          res.sendFile(response.filename);
+      });
+  } else if (req.body.theme === 'green') {
+      themeOptions = {
+          leftTextColor: "rgb(183, 217, 255)",
+          leftBackgroundColor: 'rgb(0, 119, 89)',
+          wholeBodyColor: ' rgb(rgb(139, 247, 205))',
+          rightTextColor: 'rgb(0, 119, 89)'
+      };
+
+      // HTML TO PDF CONVERTING
+      pdf.create(dynamicResume(req.body, themeOptions), options).toFile(__dirname + "/docs/" + shortName + "-resume.pdf", (error, response) => {
+        if (error) {
+          console.log(error)
+          throw Error("File is not created");
+        }
+          console.log(response.filename);
+          res.sendFile(response.filename);
+      });
+  } else {
+      // SETTING DEFAULT VALUE
+      // HTML TO PDF CONVERTING
+      pdf.create(dynamicResume(req.body, themeOptions), options).toFile(__dirname + "/docs/" + shortName + "-resume.pdf", (error, response) => {
+          if (error) {
+            console.log(error)
+            throw Error("File is not created");
+          }
+          console.log(response.filename);
+          res.sendFile(response.filename);
+      });
+  }
+
+
+});
 
   app.get("/mentoring", authenticateUser, function (req, res) {
     res.render("mentoring.ejs");
