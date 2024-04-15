b464aa837523b9ab4aa07e736edece223c9e3db3
juna1d9 <jahme014@gold.ac.uk>
2024-04-15 10:34:04 +0500

    Mentoring module + Apply for Jobs implemented

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,12 @@ const app = require("../index");
 const bcrypt = require("bcrypt");
 const dynamicResume = require('../dynamic-resume');
 const pdf = require('html-pdf');
+const multer = require('multer')
+const fs = require('fs');
+const { Console } = require("console");
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 const options = {
   "height": "10.5in",        // allowed units: mm, cm, in, px
@@ -18,10 +24,32 @@ module.exports = function (app) {
     res.redirect('/login');
   }
 
+  function hasAppliedForJob(userId, jobId, callback) {
+    const query = "SELECT * FROM job_applications WHERE ja_j_id = ? AND ja_u_id = ?";
+    db.query(query, [jobId, userId], (error, results) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      callback(null, results.length > 0); // If there are results, user has already applied, otherwise not
+    });
+  }
+
+  function countJobApplications(jobId, callback) {
+    const query = "SELECT COUNT(*) AS jobApplicationsCount FROM job_applications WHERE ja_j_id = ?";
+    db.query(query, [jobId], (error, results) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      callback(null, results[0].jobApplicationsCount); // If there are results, user has already applied, otherwise not
+    });
+  }
+
   // Handle our routes
   app.get("/", authenticateUser, function (req, res) {
     res.render("index.ejs")
-    console.log("Auth User -->",req.session.user)
+    console.log("Auth User -->", req.session.user)
   });
 
   app.get("/login", function (req, res) {
@@ -50,7 +78,7 @@ module.exports = function (app) {
             type: user.userType
           };
           res.redirect(`/`);
-          
+
         } else {
           res.render("login.ejs", {
             message: "Invalid email or password. Please try again.",
@@ -116,8 +144,13 @@ module.exports = function (app) {
 
   app.get("/jobs", authenticateUser, function (req, res) {
 
-    const query = "SELECT * FROM jobs";
-    db.query(query, (error, results) => {
+    let query;
+    if (req.session.user.type === "student") {
+      query = "SELECT * FROM jobs";
+    } else if (req.session.user.type === "employer") {
+      query = "SELECT * FROM jobs WHERE j_u_id = ?";
+    }
+    db.query(query, [req.session.user.id], (error, results) => {
       if (error) {
         throw error;
       }
@@ -125,7 +158,8 @@ module.exports = function (app) {
       if (results.length > 0) {
 
         const jobs = results;
-        res.render("jobs.ejs", {jobs: jobs});
+
+        res.render("jobs.ejs", { jobs: jobs });
 
       } else {
 
@@ -149,15 +183,79 @@ module.exports = function (app) {
       if (results.length > 0) {
 
         const job = results[0];
-        res.render("jobdetail.ejs", {job: job});
+
+
+        if (req.session.user.type == "student") {
+
+          // Check if user has applied for each job
+          hasAppliedForJob(req.session.user.id, job.j_id, (err, applied) => {
+            if (err) {
+              throw err;
+            }
+            job.already_applied = (applied) ? "yes" : "no";
+            res.render("jobdetail.ejs", { job: job });
+
+          });
+
+        } else {
+
+          countJobApplications(job.j_id, (err, count) => {
+            if (err) {
+              throw err;
+            }
+            job.jobApplicationsCount = count;
+            res.render("jobdetail.ejs", { job: job });
+
+          });
+
+        }
+
 
       } else {
 
         res.render("jobdetail.ejs", {
-          message: "Unable to find jobs!",
+          message: "No Data Found!",
         });
-        
+
       }
+    });
+
+  });
+
+  app.get("/applyforjob/:j_id", function (req, res) {
+    const jobId = req.params.j_id;
+    res.render("applyforjob.ejs", { j_id: jobId });
+  });
+
+  app.post("/applyforjob", authenticateUser, upload.single('file'), function (req, res) {
+
+    const {
+      ja_message,
+      ja_j_id
+    } = req.body;
+
+    const values = [
+      ja_j_id,
+      req.file.buffer,
+      req.session.user.id,
+      ja_message
+    ];
+
+    // Prepare SQL query to insert file into MySQL
+    const query = "INSERT INTO job_applications (ja_j_id, ja_file, ja_u_id, ja_message) VALUES (?, ?, ?, ?)";
+
+    // Execute the SQL query
+    db.query(query, values, (err, result) => {
+      if (err) {
+        res.render("applyforjob.ejs", {
+          error_message: "Application Failed! Please try again.",
+          j_id: ja_j_id
+        });
+      }
+      res.render("applyforjob.ejs", {
+        message: "Submission Successfull!",
+        j_id: ja_j_id
+      });
     });
 
   });
@@ -167,6 +265,7 @@ module.exports = function (app) {
   });
 
   app.post("/postajob", authenticateUser, function (req, res) {
+
     const {
       j_cname,
       j_email,
@@ -199,67 +298,64 @@ module.exports = function (app) {
   });
 
   app.get('/cv-builder/:theme', authenticateUser, (req, res, next) => {
-    console.log("theme: ", req.params.theme);
     switch (req.params.theme) {
-        case '1':
-            res.render('cv-builder.ejs', { theme: "blue" });
-            break;
-        case '2':
-            res.render('cv-builder.ejs', { theme: "green" });
-            break;
-        default:
-            res.render('cv-builder.ejs', { theme: "green" });
-            break;
+      case '1':
+        res.render('cv-builder.ejs', { theme: "blue" });
+        break;
+      case '2':
+        res.render('cv-builder.ejs', { theme: "green" });
+        break;
+      default:
+        res.render('cv-builder.ejs', { theme: "green" });
+        break;
     }
-});
+  });
 
-app.post('/cv-builder', authenticateUser, (req, res, next) => {
-  console.log(req.body.theme);
-  // LOWERCASE -> REMOVE SPACE -> SHORT NAME 
-  const userName = req.body.name;
-  const lowercaseName = userName.toLowerCase();
-  const noSpaceName = lowercaseName.replace(' ', '');
-  const shortName = noSpaceName.slice(0, 10);
+  app.post('/cv-builder', authenticateUser, (req, res, next) => {
+    // LOWERCASE -> REMOVE SPACE -> SHORT NAME 
+    const userName = req.body.name;
+    const lowercaseName = userName.toLowerCase();
+    const noSpaceName = lowercaseName.replace(' ', '');
+    const shortName = noSpaceName.slice(0, 10);
 
-  //Educational Info
-  const { degrees, universities, startYears, endYears } = req.body;
-  const educationalInfo = [];
+    //Educational Info
+    const { degrees, universities, startYears, endYears } = req.body;
+    const educationalInfo = [];
 
-  for (let i = 0; i < degrees.length; i++) {
-    const eduInfo = {
-      degree: degrees[i],
-      university: universities[i],
-      startYear: startYears[i],
-      endYear: endYears[i]
-    };
-    educationalInfo.push(eduInfo);
-  }
+    for (let i = 0; i < degrees.length; i++) {
+      const eduInfo = {
+        degree: degrees[i],
+        university: universities[i],
+        startYear: startYears[i],
+        endYear: endYears[i]
+      };
+      educationalInfo.push(eduInfo);
+    }
 
-  req.body.educationalInformation = educationalInfo;
-  console.log(req.body);
+    req.body.educationalInformation = educationalInfo;
+    console.log(req.body);
 
 
-  let themeOptions = {
+    let themeOptions = {
       leftTextColor: "rgb(91, 88, 255)",
       leftBackgroundColor: 'rgb(12, 36, 58)',
       wholeBodyColor: ' rgb(250, 250, 250)',
       rightTextColor: 'rgb(12, 36, 58)'
-  };
+    };
 
-  if (req.body.theme === 'blue') {
+    if (req.body.theme === 'blue') {
 
       // HTML TO PDF CONVERTING
       pdf.create(dynamicResume(req.body, themeOptions), options).toFile(__dirname + "/docs/" + shortName + "-resume.pdf", (error, response) => {
-          if (error) throw Error("File is not created");
-          console.log(response.filename);
-          res.sendFile(response.filename);
+        if (error) throw Error("File is not created");
+        res.sendFile(response.filename);
       });
-  } else if (req.body.theme === 'green') {
+    } else if (req.body.theme === 'green') {
       themeOptions = {
-          leftTextColor: "rgb(183, 217, 255)",
-          leftBackgroundColor: 'rgb(0, 119, 89)',
-          wholeBodyColor: ' rgb(rgb(139, 247, 205))',
-          rightTextColor: 'rgb(0, 119, 89)'
+        leftTextColor: "rgb(183, 217, 255)",
+        leftBackgroundColor: 'rgb(0, 119, 89)',
+        wholeBodyColor: ' rgb(rgb(139, 247, 205))',
+        rightTextColor: 'rgb(0, 119, 89)'
       };
 
       // HTML TO PDF CONVERTING
@@ -268,28 +364,110 @@ app.post('/cv-builder', authenticateUser, (req, res, next) => {
           console.log(error)
           throw Error("File is not created");
         }
-          console.log(response.filename);
-          res.sendFile(response.filename);
+        res.sendFile(response.filename);
       });
-  } else {
+    } else {
       // SETTING DEFAULT VALUE
       // HTML TO PDF CONVERTING
       pdf.create(dynamicResume(req.body, themeOptions), options).toFile(__dirname + "/docs/" + shortName + "-resume.pdf", (error, response) => {
-          if (error) {
-            console.log(error)
-            throw Error("File is not created");
-          }
-          console.log(response.filename);
-          res.sendFile(response.filename);
+        if (error) {
+          console.log(error)
+          throw Error("File is not created");
+        }
+        res.sendFile(response.filename);
       });
-  }
+    }
 
 
-});
+  });
 
   app.get("/mentoring", authenticateUser, function (req, res) {
     res.render("mentoring.ejs");
   });
+
+  app.post("/mentoring", authenticateUser, upload.single('file'), function (req, res) {
+
+    const {
+      m_name,
+      m_email,
+      m_phone,
+      m_enquiry,
+    } = req.body;
+
+    const values = [
+      m_name,
+      m_email,
+      m_phone,
+      m_enquiry,
+      req.file.buffer,
+      req.session.user.id
+    ];
+
+    // Prepare SQL query to insert file into MySQL
+    const query = "INSERT INTO mentorings (m_name, m_email, m_phone, m_enquiry, m_file, m_u_id) VALUES (?, ?, ?, ?, ?, ?)";
+
+    // Execute the SQL query
+    db.query(query, values, (err, result) => {
+      if (err) {
+        res.render("mentoring.ejs", {
+          error_message: "Mentoring Request Failed! Please try again.",
+        });
+      }
+      res.render("mentoring.ejs", {
+        message: "Submission Successfull!",
+      });
+    });
+
+  });
+
+  app.get("/viewmentoring", authenticateUser, function (req, res) {
+
+    const query = "SELECT * FROM mentorings";
+    db.query(query, (error, results) => {
+      if (error) {
+        throw error;
+      }
+
+      if (results.length > 0) {
+
+        const mentorings = results;
+        res.render("viewmentoring.ejs", { mentorings: mentorings });
+
+      } else {
+
+        res.render("viewmentoring.ejs", {
+          message: "No Data Found!",
+        });
+
+      }
+    });
+
+  });
+
+  app.get('/file/:id', (req, res) => {
+    const fileId = req.params.id;
+
+    // Fetch blob data from MySQL based on fileId
+    db.query('SELECT m_name, m_file FROM mentorings WHERE m_id = ?', [fileId], (err, results) => {
+      if (err) {
+        console.error('Error fetching file data:', err);
+        return res.status(500).send('Error fetching file data from database.');
+      }
+
+      if (results.length === 0) {
+        return res.status(404).send('File not found.');
+      }
+
+      res.setHeader('Content-Disposition', `attachment; filename="Attachment.pdf"`);
+
+      // Set appropriate content type based on the file type
+      res.setHeader('Content-Type', 'application/pdf');
+
+      // Send the blob data as the response
+      res.send(results[0].m_file);
+    });
+  });
+
 
   app.get("/training", authenticateUser, function (req, res) {
     res.render("training.ejs");
